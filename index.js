868576ff3c6039188bd7270fa00ac4a788f86f7c
miraballesramiro <124004198+miraballesramiro@users.noreply.github.com>
2023-09-25 20:12:34 -0300

    Corección PreEntrega2

--- index.js
+++ index.js
@@ -1,87 +1,82 @@
-const numbers = document.querySelectorAll('.numbers');
-const result = document.querySelector('.result span');
-const signs = document.querySelectorAll('.item3.sign');
-const equals = document.querySelector('.equal');
-const clear = document.querySelector('.clear');
-const negative = document.querySelector('.negative');
-const percent = document.querySelector('.percent');
+const productos = [
+  { nombre: "Harina", precio: 50 },
+  { nombre: "Galletas", precio: 100 },
+  { nombre: "Cerveza", precio: 150 },
+  { nombre: "Leche", precio: 200 },
+  { nombre: "Gaseosa", precio: 250 },
+];
+let carrito = [];
 
-let firstValue = "";
-let isFirstValue = false;
-let secondValue = "";
-let isSecondValue = false;
-let sign = "";
-let resultValue = 0;
+let seleccion = prompt("Hola! ¿Desea comprar algún producto?");
 
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', (e) => {
-        let atr = e.target.textContent;
-        if (isFirstValue === false) {
-            getFirstValue(atr);
-        }
-        if (isSecondValue == false) {
-            getSecondValue(atr);
-        }
-    });
+while (seleccion != "Sí" && seleccion != "No") {
+  alert("Por favor, ingresar sí o no");
+  seleccion = prompt("Hola! ¿Deasea comprar algo?");
 }
 
-
-function getNumber() {
-    return paraseFloat(prompt("Ingrese un número:"));
+if ((seleccion = "Sí")) {
+  alert("A continuación, nuestra lista de productos");
+  let todoslosProductos = productos.map(
+    (producto) => producto.nombre + " " + producto.precio + "$"
+  );
+  alert(todoslosProductos.join(" - "));
+} else if (seleccion == "No") {
+  alert("Gracias por venir. ¡Hasta pronto!");
 }
 
-function sumar(a, b) {
-    return a + b;
-}
+while (seleccion != "No") {
+  let producto = prompt("Agregá un producto a tu carrito");
+  let precio = 0;
 
-function restar(a, b) {
-    return a - b;
-}
-
-function multiplicar(a, b) {
-    return a * b;
-}
-
-function dividir(a, b) {
-    if (b !== 0) {
-        return a / b;
-    } else {
-        return "Error: No se puede dividir por cero.";
+  if (
+    producto == "Harina" ||
+    producto == "Galletas" ||
+    producto == "Cerveza" ||
+    producto == "Leche" ||
+    producto == "Gaseosa"
+  ) {
+    switch (producto) {
+      case "Harina":
+        precio = 50;
+        break;
+      case "Galletas":
+        precio = 100;
+        break;
+      case "Cerveza":
+        precio = 150;
+        break;
+      case "Leche":
+        precio = 200;
+        break;
+      case "Gaseosa":
+        precio = 250;
+        break;
+      default:
+        break;
     }
+    let unidades = parseInt(prompt("¿Cuántas unidades quiere llevar?"))
+
+    carrito.push({producto, unidades, precio});
+    console.log(carrito)
+  } else {
+    alert("No tenemos ese producto")
+  }
+
+  seleccion = prompt("¿Desea seguir comprando?")
+
+  while(seleccion === "No"){
+    alert("¡Gracias por comprar! ¡Hasta pronto!");
+    carrito.forEach((carritoFinal) => {
+      console.log(
+        `producto: ${carritoFinal.producto}, unidades: ${
+          carritoFinal.unidades
+        },total a pagar por producto ${
+          carritoFinal.unidades * carritoFinal.precio
+        }`);
+    });
+    break;
+  }
 }
 
-function calcularPorcentaje(numero, porcentaje) {
-    return (numero * porcentaje) / 100;
-}
-
-const operacion = prompt("Ingrese la operación que desea realizar (+, -, *, /, %):");
-
-switch (operacion) {
-    case "+":
-        const numero1 = getNumber();
-        const numero2 = getNumber();
-        const resultadoSuma = sumar(numero1, numero2);
-        alert('El resultado de la suma es: ${resultadoSuma}');
-        break;
-    case "-":
-        const minuendo = getNumber();
-        const sustraendo = getNumber();
-        const resultadoResta = restar(minuendo, sustraendo);
-        alert('El resultado de la multiplicación es: ${resultadoMultiplicacion}');
-        break;
-    case "/":
-        const dividendo = getNumber();
-        const divisor = getNumber ();
-        const resultadoDivision = dividir(dividiendo, divisor);
-        alert(resultadoDivision);
-        break;
-    case "%":
-        const numero = getNumber();
-        const porcentaje = getNumber();
-        const resultadoPorcentaje = calcularPorcentaje(numero, porcentaje);
-        alert('El ${porcentaje}% de ${numero} es: ${ressultadoPorcentaje}');
-        break;
-    default:
-        alert("Operación no válida.");
-        break;
-}
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+console.log(`El total a pagar por su compra es: ${total}`)
